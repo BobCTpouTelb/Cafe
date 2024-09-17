7b32ff7bb059b5f4be28721a99463dd34c0efe6e
Bob_CTpOuTelb <mikle0373@gmail.com>
2024-09-17 10:40:33 +0300

    Привязал БД, добавил авторизацию, начал работать над главным окном

--- Сafe/App.xaml.cs
+++ Сafe/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Сafe.Model;
 
 namespace Сafe
 {
@@ -13,5 +14,6 @@ namespace Сafe
     /// </summary>
     public partial class App : Application
     {
+        public static CafeEntities context = new CafeEntities();
     }
 }
---
+++ Сafe/View/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Сafe.Model;
+
+namespace Сafe.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTb.Text) && string.IsNullOrEmpty(PasswordPb.Password))
+            {
+                MessageBox.Show("Введите логин и пароль");
+            }
+            else if (string.IsNullOrEmpty(LoginTb.Text) || string.IsNullOrEmpty(PasswordPb.Password))
+            {
+                MessageBox.Show("Введите логин и пароль");
+            }
+            else
+            {
+                // Проверка данных, поиск пользователя в таблице БД
+                Employee newEmployee = App.context.Employee.FirstOrDefault(employee => employee.Login == LoginTb.Text && employee.Password == PasswordPb.Password);
+                
+                if(newEmployee != null)
+                {
+                    MainWindow mainWindow = new MainWindow();
+
+                    // Сотрудник найден
+                    switch (newEmployee.RoleId)
+                    {
+                        case 1:
+                            // Открываем страницу администратора
+                            mainWindow.Show();
+                            break;
+                        case 2:
+                            // Открываем страницу официанта
+                            mainWindow.Show();
+                            break;
+                        case 3:
+                            // Открываем страницу повара
+                            mainWindow.Show();
+                            break;
+                    }
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не найден!");
+                }
+            }
+        }
+    }
+}
