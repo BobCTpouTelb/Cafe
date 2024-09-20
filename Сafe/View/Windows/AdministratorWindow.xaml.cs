f536c288eae8d60089d17cd7afb8e53be1b09f72
Bob_CTpOuTelb <mikle0373@gmail.com>
2024-09-20 16:30:21 +0300

    Начал делать окно администратора

---
+++ Сafe/View/Windows/AdministratorWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Сafe.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdministratorWindow.xaml
+    /// </summary>
+    public partial class AdministratorWindow : Window
+    {
+        public AdministratorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeListBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OrderListBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShiftListBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
